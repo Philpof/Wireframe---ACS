8cea73e2c98f6ab75a70012157d8ccb7ffe44904
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-11 08:27:53 +0200

    Préparation

--- header.php
+++ header.php
@@ -13,29 +13,29 @@
 <body>
   <!-- Code de "navbar" pris sur bootstrap -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" target="_blank" href="https://www.accesscodeschool.fr/"><img src="img/logo-acs-black.png" alt="logo access code school" id="logo-acs"></a>
+    <a class="navbar-brand" target="_blank" href="https://www.accesscodeschool.fr/"><img src="xxxx" alt="Logo Navbar" id="logo-navbar"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="xxxx">Accueil <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="html.php">HTML</a>
+          <a class="nav-link" href="xxxx">1</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="css.php">CSS</a>
+          <a class="nav-link" href="xxxx">2</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="console.php">Command Line</a>
+          <a class="nav-link" href="xxxx">3</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="git.php">Git & GitHub</a>
+          <a class="nav-link" href="xxxx">4</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="atom.php">Atom</a>
+          <a class="nav-link" href="xxxx">5</a>
         </li>
       </ul>
     </div>
