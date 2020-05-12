c0ca750dd79d78a9f7cf8a80d090d1d2ef58f2a9
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-12 16:40:05 +0200

    Page en construction

---
+++ noway.php
@@ -0,0 +1,10 @@
+<?php include "header.php";?>
+
+<div class="ContainerNoWay">
+  <h1>Page en construction</h1>
+  <p>Merci de bien vouloir revenir plus tard</p>
+  <p>Cliquez sur l'image ci-dessous pour revenir au menu principal</p>
+  <a href="index.php"><img src="img\En_Construction.jpg" alt="En Construction"></a>
+</div>
+
+<?php include "footer.php";
