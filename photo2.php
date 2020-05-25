8fb67d1a4dde58fa76bbfe2388da0f12f2008257
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-25 09:09:02 +0200

    Evolution 2

--- photo2.php
+++ photo2.php
@@ -4,7 +4,7 @@
   <h1>Page en construction</h1>
   <p>Merci de bien vouloir revenir plus tard</p>
   <p>Cliquez sur la photo ci-dessous pour revenir aux photos</p>
-  <a href="index.php#section4"><img src="img\faun_template_2" alt="Photo n°2"></a>
+  <a href="index.php#section4"><img src="img\faun_template_2.png" alt="Photo n°2"></a>
 </div>
 
 <?php include "footer.php";
